42b0f8b3ef190a9cd79d56f1066c0b73f38e2a7b
Pedro Ribeiro <pedro.alex.ribeiro@hotmail.com>
2019-11-02 23:17:37 +0000

    Application module to use as header

---
+++ projeto2-parent/projeto2-web/src/main/java/servlet/Application.java
@@ -0,0 +1,34 @@
+
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import data.*;
+
+public class Application extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void header(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        HttpSession session = request.getSession(false);  
+        if(session!=null){  
+            User user = (User)session.getAttribute("user");
+            out.println("<a href=/projeto2-web/profile>Profile</a> |  <a href=/projeto2-web/logout>Logout</a>");
+            out.println("<hr>");
+            out.println("Welcome " + user.getName() + "!");
+            
+        }else{
+            out.println("<a href=/projeto2-web/login>Login</a> |  <a href=/projeto2-web/register>Register</a>");
+            out.println("<hr>");
+        }
+    }
+}
