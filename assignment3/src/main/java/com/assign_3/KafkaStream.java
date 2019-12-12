23fe6729b50caf5f60375f00fdbd8337ee257502
KinryuBarao <s.franciscobarao@gmail.com>
2019-12-12 17:37:33 +0000

    kafka?

--- assignment3/src/main/java/com/assign_3/KafkaStream.java
+++ assignment3/src/main/java/com/assign_3/KafkaStream.java
@@ -1,26 +1,16 @@
 package com.assign_3;
 
 import java.util.Properties;
-import java.util.Random;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.time.Duration;
-import java.util.Arrays;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
+
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KGroupedStream;
-import org.apache.kafka.streams.kstream.KGroupedTable;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.KeyValueMapper;
-import org.apache.kafka.streams.kstream.ValueMapper;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.streams.kstream.Produced;
 
 public class KafkaStream {
     /*
@@ -28,23 +18,26 @@ public class KafkaStream {
      * other kafka streams
      */
     public static void main(String[] args) throws Exception {
-
+        System.out.println("Kafka Stream");
         // Kafka consumer configuration settings
         String topicSales = "Sales";
         String topicPurchases = "Purchases";
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("group.id", "test");
-        props.put("enable.auto.commit", "true");
-        props.put("auto.commit.interval.ms", "1000");
-        props.put("session.timeout.ms", "30000");
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        String outputTopic = "resultsTopic";
+
+        java.util.Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "app");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
         StreamsBuilder builder = new StreamsBuilder();
 
-        // id , 10 10 Spain
         KStream<String, String> salesStream = builder.stream(topicSales);
+        salesStream.foreach((k, v) -> {
+            System.out.println("K: " + k + " v: " + v);
+        });
+
+        // id , 10 10 Spain
         KStream<String, String> purchasesStream = builder.stream(topicPurchases);
 
         KTable<String, Double> revenueTable = salesStream.mapValues(v -> transform(v))
@@ -61,8 +54,10 @@ public class KafkaStream {
         KTable<String, Double> totalRevenue = s.reduce((v1, v2) -> {
             return (v1 + v2);
         });
-        System.out.println("\n Total revenue: " + totalRevenue);
+        revenueTable.mapValues(v -> v).toStream().to(outputTopic, Produced.with(Serdes.String(), Serdes.Double()));
 
+        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        streams.start();
         // Total profit needs a groupBy(k,v -> null)
         /*
          * // second step: compute average for each 2-tuple final KTable<String,Double>
