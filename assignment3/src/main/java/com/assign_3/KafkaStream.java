b67e03633a82566bc94942b32f817d1b15267b92
Pedro Ribeiro <pedro.alex.ribeiro@hotmail.com>
2019-12-12 23:34:13 +0000

    total and window working, WIP aggregate

--- assignment3/src/main/java/com/assign_3/KafkaStream.java
+++ assignment3/src/main/java/com/assign_3/KafkaStream.java
@@ -1,5 +1,6 @@
 package com.assign_3;
 
+import java.time.Duration;
 import java.util.Properties;
 
 import org.apache.kafka.common.serialization.Serdes;
@@ -7,13 +8,19 @@ import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.Aggregator;
 import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.Initializer;
 import org.apache.kafka.streams.kstream.KGroupedStream;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.SessionWindowedKStream;
+import org.apache.kafka.streams.kstream.SessionWindows;
+import org.apache.kafka.streams.kstream.Windowed;
 
 public class KafkaStream {
+    static final long TIME_GAP = Duration.ofMinutes(2).toMillis();
     /*
      * Thread for producer Talk to prof about parsing info from DB and to Send to
      * other kafka streams
@@ -45,32 +52,62 @@ public class KafkaStream {
             return (valueRevennue - valueExpenses);
         });
 
-        KGroupedStream<String, Double> s = revenueTable.toStream().groupBy((k, v) -> "");
-        KTable<String, Double> totalRevenue = s.reduce((v1, v2) -> {
+        KGroupedStream<String, Double> revenue_group = revenueTable.toStream().groupBy((k, v) -> "");
+        KTable<String, Double> totalRevenue = revenue_group.reduce((v1, v2) -> {
             return (v1 + v2);
         });
-<<<<<<< HEAD
+
+        KGroupedStream<String, Double> expense_group = expensesTable.toStream().groupBy((k, v) -> "");
+        KTable<String, Double> totalExpense = expense_group.reduce((v1, v2) -> {
+            return (v1 + v2);
+        });
         //totalRevenue.toStream().map((k, v) -> new KeyValue<>(k, "" + v)).to(outputTopic, Produced.with(Serdes.String(), Serdes.String()));
-=======
+
+        // Grouped by time
+        SessionWindowedKStream<String,Double> revenue_window = revenueTable.toStream().groupBy((k, v) -> "").windowedBy(SessionWindows.with(TIME_GAP));
+        KTable<Windowed<String>, Double> totalWindowRev = revenue_window.reduce((v1, v2) -> {
+            return (v1 + v2);
+        });
+
+        SessionWindowedKStream<String,Double> expense_window = expensesTable.toStream().groupBy((k, v) -> "").windowedBy(SessionWindows.with(TIME_GAP));
+        KTable<Windowed<String>, Double> totalWindowExp = expense_window.reduce((v1, v2) -> {
+            return (v1 + v2);
+        });
+        totalRevenue.toStream().foreach((k, v) -> {
+            System.out.println("Total something: " + k + " " + v);
+        });
+
+        totalWindowRev.toStream().foreach((k, v) -> {
+            System.out.println("Total Window something: " + k + " " + v);
+        });
+
 
         java.util.Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "app");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
->>>>>>> f111fc7d7c9ef2040316d5d5f55452fb2562872a
 
         KafkaStreams streams = new KafkaStreams(builder.build(), props);
         streams.start();
         // Total profit needs a groupBy(k,v -> null)
-        /*
-         * // second step: compute average for each 2-tuple final KTable<String,Double>
-         * average = countAndSum.mapValues( new ValueMapper<Tuple2<Long, Long>,
-         * Double>() {
-         * 
-         * @Override public Double apply(Tuple2<Long, Long> value) { return value.value2
-         * / (double) value.value1; } });
-         */
+        /* final KTable<String,Tuple2<Long,Long>> countAndSum = revenue_group
+                                                            .aggregate(
+                                                                new Initializer<Tuple2<Long, Long>>() {
+                                                                    @Override
+                                                                    public Tuple2<Long, Long> apply() {
+                                                                        return new Tuple2<>(0L, 0L);
+                                                                    }
+                                                                },
+                                                                new Aggregator<String, String, Tuple2<Long, Long>>() {
+                                                                    @Override
+                                                                    public Tuple2<Long, Long> apply(final String key, final Long value, final Tuple2<Long, Long> aggregate) {
+                                                                            ++aggregate.value1;
+                                                                            aggregate.value2 += value;
+                                                                            return aggregate;
+                                                                    }
+                                                                }   );
+        */
 
         // Calcular medias -> aggregate - fazer soma e contar quantos ja ocorreram ...
         // ("total, counter")
@@ -84,5 +121,15 @@ public class KafkaStream {
         Double j = Double.parseDouble(parts[1]);
 
         return i * j;
+    }
+
+    class Tuple2<T1, T2> {
+        public T1 value1;
+        public T2 value2;
+        
+        Tuple2(T1 v1, T2 v2) {
+            value1 = v1;
+            value2 = v2;
+        }
     }
 }
