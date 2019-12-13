5438bb87d17433c8a2a63be1899b5fb59ae1c03e
Pedro Ribeiro <pedro.alex.ribeiro@hotmail.com>
2019-12-13 14:41:52 +0000

    organized code

--- assignment3/src/main/java/com/assign_3/KafkaStream.java
+++ assignment3/src/main/java/com/assign_3/KafkaStream.java
@@ -2,24 +2,15 @@ package com.assign_3;
 
 import java.time.Duration;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.Aggregator;
 import org.apache.kafka.streams.kstream.Grouped;
-import org.apache.kafka.streams.kstream.Initializer;
-import org.apache.kafka.streams.kstream.Joined;
 import org.apache.kafka.streams.kstream.KGroupedStream;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Materialized;
-import org.apache.kafka.streams.kstream.Produced;
-import org.apache.kafka.streams.kstream.SessionWindowedKStream;
-import org.apache.kafka.streams.kstream.SessionWindows;
 import org.apache.kafka.streams.kstream.TimeWindowedKStream;
 import org.apache.kafka.streams.kstream.TimeWindows;
 import org.apache.kafka.streams.kstream.Windowed;
@@ -50,103 +41,48 @@ public class KafkaStream {
                     return v1 + v2;
                 });
 
-        revenueTable.toStream().foreach((k, v) -> {
-            System.out.println("revenue something: " + k + " " + v);
-        });
-
-        //revenueTable.toStream().map((k, v) -> new KeyValue<String, String>("", tDatabase("revenue", k, v)))
-        //        .to("results", Produced.with(Serdes.String(), Serdes.String()));
-
+        // Expenses
         KTable<String, Double> expensesTable = purchasesStream.mapValues(v -> transform(v))
                 .groupByKey(Grouped.with(Serdes.String(), Serdes.Double())).reduce((v1, v2) -> v1 + v2);
 
-        expensesTable.toStream().foreach((k, v) -> {
-            System.out.println("expenses something: " + k + " " + v);
-        });
-
+        // Profit
         KTable<String, Double> profitTable = revenueTable.join(expensesTable, (valueRevennue, valueExpenses) -> {
             return (valueRevennue - valueExpenses);
         });
 
-<<<<<<< HEAD
-        // Total revenue/profit/expense
-=======
-        profitTable.toStream().foreach((k, v) -> {
-            System.out.println("profit something: " + k + " " + v);
-        });
-
->>>>>>> c401918b2f8c4c2c6241343376e3956f00ecb93b
+        // Total revenue
         KGroupedStream<String, Double> revenue_group = revenueTable.toStream().groupBy((k, v) -> "");
         KTable<String, Double> totalRevenue = revenue_group.reduce((v1, v2) -> {
             return (v1 + v2);
         });
-        totalRevenue.toStream().foreach((k, v) -> {
-            System.out.println("Total revenue: " + k + " " + v);
-        });
 
+        // Total expense
         KGroupedStream<String, Double> expense_group = expensesTable.toStream().groupBy((k, v) -> "");
         KTable<String, Double> totalExpense = expense_group.reduce((v1, v2) -> {
             return (v1 + v2);
         });
-        totalExpense.toStream().foreach((k, v) -> {
-            System.out.println("Total expense: " + k + " " + v);
-        });
 
+        // Total profit
         KGroupedStream<String, Double> profit_group = profitTable.toStream().groupBy((k, v) -> "");
         KTable<String, Double> totalProfit = profit_group.reduce((v1, v2) -> {
             return (v1 + v2);
         });
 
-        // totalRevenue.toStream().map((k, v) -> new KeyValue<>(k, "" +
-        // v)).to(outputTopic, Produced.with(Serdes.String(), Serdes.String()));
-
-        // Grouped by time
+        // Timed window for revenue
         TimeWindowedKStream<String, Double> revenue_window = revenueTable.toStream().groupBy((k, v) -> "")
                 .windowedBy(TimeWindows.of(TIME_GAP));
         KTable<Windowed<String>, Double> totalWindowRev = revenue_window.reduce((v1, v2) -> {
             return (v1 + v2);
         });
 
+        // Timed window for expenses
         TimeWindowedKStream<String, Double> expense_window = expensesTable.toStream().groupBy((k, v) -> "")
                 .windowedBy(TimeWindows.of(TIME_GAP));
         KTable<Windowed<String>, Double> totalWindowExp = expense_window.reduce((v1, v2) -> {
             return (v1 + v2);
         });
-<<<<<<< HEAD
-        totalWindowRev.toStream().foreach((k, v) -> {
-            // System.out.println("Total Window something: " + k + " " + v);
-        });
-=======
 
-        /* totalRevenue.toStream().foreach((k, v) -> {
-            System.out.println("Total something: " + k + " " + v);
-        });
-
-        totalWindowRev.toStream().foreach((k, v) -> {
-            System.out.println("Total Window something: " + k + " " + v);
-        }); */
->>>>>>> c401918b2f8c4c2c6241343376e3956f00ecb93b
-
-        // totalRevenue.toStream().map((k, v) -> new KeyValue<>(k, "" +
-        // v)).to(outputTopic, Produced.with(Serdes.String(), Serdes.String()));
-
-        // ("total, counter")
-        // agreggate passa id, value,"total,count"
-
-        java.util.Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "app");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-
-        KafkaStreams streams = new KafkaStreams(builder.build(), props);
-        streams.start();
-        // Total profit needs a groupBy(k,v -> null)
-
-        // Calcular medias -> aggregate - fazer soma e contar quantos ja ocorreram ...
-        // ("total, counter")
-        // agreggate passa id, value,"total,count"
-
+        // Mean per item
         KTable<String, String> medianPerItem = purchasesStream.mapValues(v -> transform(v))
                 .groupByKey(Grouped.with(Serdes.String(), Serdes.Double()))
                 .aggregate(() -> "0,0", (id, newVal, aggVal) -> {
@@ -156,6 +92,7 @@ public class KafkaStream {
                     return (val + "," + count);
                 });
 
+        // Total Mean
         KTable<String, String> medianTotal = purchasesStream.mapValues(v -> transform(v)).groupBy((k, v) -> "")
                 .aggregate(() -> "0,0", (id, newVal, aggVal) -> {
                     String parts[] = aggVal.split(",");
@@ -163,6 +100,25 @@ public class KafkaStream {
                     int count = Integer.parseInt(parts[1]) + 1;
                     return (val + "," + count);
                 });
+
+        // totalRevenue.toStream().map((k, v) -> new KeyValue<>(k, "" +
+        // v)).to(outputTopic, Produced.with(Serdes.String(), Serdes.String()));
+
+        // totalRevenue.toStream().map((k, v) -> new KeyValue<>(k, "" +
+        // v)).to(outputTopic, Produced.with(Serdes.String(), Serdes.String()));
+
+         //revenueTable.toStream().map((k, v) -> new KeyValue<String, String>("", tDatabase("revenue", k, v)))
+        //        .to("results", Produced.with(Serdes.String(), Serdes.String()));
+
+        // Properties for streams
+        java.util.Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "app");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        streams.start();
     }
 
     private static Double transform(String s) {
@@ -176,15 +132,5 @@ public class KafkaStream {
     public static String tDatabase(String type, String id, Double value) {
         return "{\"schema\":{\"type\":\"struct\",\"fields\":[{\"type\":\"string\",\"optional\":false,\"field\":\"data_type\"},{\"type\":\"double\",\"optional\":false,\"field\":\"value\"},{\"type\":\"integer\",\"optional\":false,\"field\":\"item_id\"}],\"optional\":false,\"name\":\"total data\"},\"payload\":{\"data_type\":\""
                 + type + "\", \"value\":\"" + value + "\",\"item_id\":\"" + id + "\"}}";
-    }
-
-    class Tuple2<T1, T2> {
-        public T1 value1;
-        public T2 value2;
-
-        Tuple2(T1 v1, T2 v2) {
-            value1 = v1;
-            value2 = v2;
-        }
     }
 }
